70cdda574b083d10f98cf87f7ad384c0a6b66ca6
sipolai-user <user@sipolai.local>
2026-07-23 14:35:47 +0900

    Update Cloudinary config to support CLOUDINARY_URL

--- app/Config/Cloudinary.php
+++ app/Config/Cloudinary.php
@@ -12,8 +12,19 @@ class Cloudinary extends BaseConfig
 
     public function __construct()
     {
-        $this->cloudName = getenv('CLOUDINARY_CLOUD_NAME');
-        $this->apiKey = getenv('CLOUDINARY_API_KEY');
-        $this->apiSecret = getenv('CLOUDINARY_API_SECRET');
+        $cloudinaryUrl = getenv('CLOUDINARY_URL');
+        if (!empty($cloudinaryUrl)) {
+            // Parse CLOUDINARY_URL: cloudinary://API_KEY:API_SECRET@CLOUD_NAME
+            $parsed = parse_url($cloudinaryUrl);
+            if (isset($parsed['user'], $parsed['pass'], $parsed['host'])) {
+                $this->apiKey = $parsed['user'];
+                $this->apiSecret = $parsed['pass'];
+                $this->cloudName = $parsed['host'];
+            }
+        } else {
+            $this->cloudName = getenv('CLOUDINARY_CLOUD_NAME');
+            $this->apiKey = getenv('CLOUDINARY_API_KEY');
+            $this->apiSecret = getenv('CLOUDINARY_API_SECRET');
+        }
     }
 }
